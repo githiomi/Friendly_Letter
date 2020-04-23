7829ef25daff0c1403de7d0679c75985f79816eb
Dhosio <danielgithiomi@gmail.com>
2020-04-23 15:26:46 +0300

    This will show the user the favorite phots page using handlebars templates

--- src/main/java/AppFriendlyLetter.java
+++ src/main/java/AppFriendlyLetter.java
@@ -17,18 +17,9 @@ public class AppFriendlyLetter {
             return new ModelAndView(new HashMap(), "hello.hbs");
                 }, new HandlebarsTemplateEngine());
 
-        get("/photos", (request, response) ->
-                "<!DOCTYPE html>" +
-                        "<html>" +
-                        "<head>" +
-                        "<title>Hello Friend!</title>" +
-                        "<link rel='stylesheet'  href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-                        "</head>" +
-                        "<body>" +
-                        "<h1>Favorite Traveling Photos</h1>" +
-                        "<ul>" +
-                        "<li><img src='/images/image1.jpg' alt='A photo of a developer.' style = 'margin : '100px;' width = '250px' height = '250px'/></li>" +
-                        "<li><img src='/images/image2.png' alt='A photo of another developer.' style = 'margin : 50px;' width = '250px' height = '250px' /></li>" + "</ul>" + "</body>" + "</html>" );
+        get("/photos", (request, response) -> {
+            return new ModelAndView(new HashMap(), "dev_photos.hbs");
+                }, new HandlebarsTemplateEngine());
 
     }
 }
